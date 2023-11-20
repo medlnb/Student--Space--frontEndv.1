3be20c4a035c21519ea550a8f1952d7224971343
Lanabi Mohamed <127102977+medlnb@users.noreply.github.com>
2023-11-20 16:31:23 +0100

    Add loading page to TasksPage

--- src/Components/TaskPage/TaskPage.tsx
+++ src/Components/TaskPage/TaskPage.tsx
@@ -1,12 +1,14 @@
 import { useContext } from "react"
 import { TasksContext } from "../../Contexts/TaskContext"
 import './TaskPage.css'
+import PropagateLoader from "react-spinners/PropagateLoader"
 
 function TaskPage() {
-  
+
   const { state } = useContext(TasksContext)
   if (!state)
     return
+
   const Announcements = state.map((element, index) => (
     <div
       className="Task--container"
@@ -25,7 +27,17 @@ function TaskPage() {
     <div className='sub--main--container'>
       <h1 className='sub--main--title'>Tasks</h1>
       <div className='tasks--container'>
-        {Announcements}
+        {state[0]._id === "default_value" ?
+          <div className='loader--container'>
+            <PropagateLoader
+              color={"white"}
+              loading={true}
+              size={20}
+            />
+          </div>
+          :
+          Announcements
+        }
       </div>
     </div>
   )
