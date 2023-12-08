354283c579a9331de94758947d621a9b75994c40
Lanabi Mohamed <127102977+medlnb@users.noreply.github.com>
2023-12-08 19:51:31 +0100

    Light mode Added

--- src/Contexts/Theme.tsx
+++ src/Contexts/Theme.tsx
@@ -1,13 +1,16 @@
 import { createContext, useState } from "react";
 
 export const DarkModeContext = createContext<{ DarkMode: boolean; setDarkMode: React.Dispatch<React.SetStateAction<boolean>>; }>({
-  DarkMode: true,
+  DarkMode: !!localStorage.getItem("darkmode"),
   setDarkMode: () => {}
 });
 
 export const DarkModeContextProvider = ({ children }: any) => {
-
-  const [DarkMode, setDarkMode] = useState<boolean>(true)
+  const [DarkMode, setDarkMode] = useState<boolean>(!!localStorage.getItem("darkmode"))
+  if (DarkMode)
+    localStorage.setItem("darkmode", "True")
+  else
+    localStorage.removeItem("darkmode")
   return (
     <DarkModeContext.Provider value={{ DarkMode, setDarkMode }}>
       {children}
